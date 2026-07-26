9cdb34c8120a006307f93b66b821dac363f3760d
Dushyant Rajput <dushyantr737@gmail.com>
2026-07-26 11:29:43 +0530

    Day -9 solved module arithmetic problem

---
+++ Number-Theory/problem-3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+
+    cout << "7\n";
+}
+int main()
+{
+ int t;
+ cin>>t;
+ while (t--)
+ {
+    /* code */
+    solve();
+ }
+ 
+ return 0;
+}
